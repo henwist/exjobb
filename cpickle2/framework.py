3e117ac95e2f30dd95e43c6d8e6bef5be9024a12
Henrik Wistbacka <S142066@student.hb.se>
2017-02-19 22:25:33 +0000

    Now cpickle2 has a working db with one sensor.

--- cpickle2/framework.py
+++ cpickle2/framework.py
@@ -1,21 +1,17 @@
 import abc
 import smbus
 import time
-import BMP280_I2C_smbus as bmp280
-
-BMP280_I2CADDR = 0x77
-BMP280_DIG_T1 = 0x88  # R   Unsigned Calibration data (16 bits)
 
 class AbstractBus:
   __metaclass__ = abc.ABCMeta
 
   @abc.abstractmethod
   def read(self):
-   raise NotImplementedError("Class %s doesn't implement " %(self.__class__.__name__))
+   raise NotImplementedError("Class %s  needs to implement " %(self.__class__.__name__))
 
   @abc.abstractmethod
   def write(self):
-   raise NotImplementedError("Class %s doesn't implement " %(self.__class__.__name__))
+   raise NotImplementedError("Class %s needs to implement " %(self.__class__.__name__))
 
 
 
@@ -43,7 +39,7 @@ class Framework():
 	  self.buses[bus_type] = SPIBus(bus_nr)
 
 	if bus_type == 'uart':
-	  self.buses[bus_type] = SPIBus(bus_nr)
+	  self.buses[bus_type] = UARTBus(bus_nr)
 	
 	if bus_type != None and bus_instance != None:
 	  self.buses[bus_type] = bus_instance
@@ -51,16 +47,5 @@ class Framework():
   def read_bus(self, bus_type, address, register, byte_count, *args):
 	return self.buses[bus_type].read(address, register, byte_count, *args)
 
-  def write_bus(self, bus_type, address, register, value_list):
-	return self.buses[bus_type].write(address, register, value_list)
-
-
-#a = Framework('i2c')
-#print "reg 0x88, 0x89 values = ", a.read_bus('i2c', BMP280_I2CADDR, BMP280_DIG_T1, 2)
-
-#sensor = bmp280.BMP280(framework=a)
-
-
-#print "Temp = {0:0.2f}".format(sensor.read_temperature())
-#print "Pressure = {0:0.2f}".format(sensor.read_pressure())
-#print "Alt = {0:0.2f}".format(sensor.read_altitude(sealevel_pa=101800))
+  def write_bus(self, bus_type, address, register, value_list, *args):
+	return self.buses[bus_type].write(address, register, value_list, *args)
---
+++ cpickle2/mycpickle.py
@@ -0,0 +1,38 @@
+import framework
+import sqlite3
+
+conn = sqlite3.connect('sensor.db')
+
+c = conn.cursor()
+
+#c.execute('''CREATE TABLE Module (module_name text NOT NULL PRIMARY KEY, module_code blob NOT NULL)''')
+#c.execute('''CREATE TABLE Sensor (python blob)''')
+
+
+#str_mod = open('BMP280_I2C_smbus.py', 'rb').read()
+
+#c.execute('INSERT INTO Module VALUES (?,?)', ('BMP280_Adafruit', str_mod))
+#conn.commit()
+
+c.execute('SELECT module_code FROM Module WHERE module_name=\'BMP280_Adafruit\';')
+
+conn.commit()
+
+module = c.fetchone()
+
+print module[0]
+
+exec(module[0])
+ 
+bsensor = BMP280()
+
+frame = framework.Framework(bus_type='i2c', bus_nr=1, bus_instance=None)
+bsensor.tie_framework(frame, 'i2c')
+
+print "bsensor temp: ", bsensor.read_temperature()
+
+print "bsensor press: ", bsensor.read_pressure()
+
+print "bsensor alt: {0:0.2f}".format(bsensor.read_altitude(sealevel_pa=100740))
+
+conn.close()
