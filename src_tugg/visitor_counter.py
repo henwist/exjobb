0f4530b9e9add1b23eccc8c766bc452874e25112
Rasmus Lundquist <Rasmus Lundquist>
2017-02-23 11:29:37 +0100

    Lade in koden som vi pratade om i skolan i visitor_counter

--- src_tugg/visitor_counter.py
+++ src_tugg/visitor_counter.py
@@ -1,1 +1,22 @@
-#use https://pypi.python.org/pypi/hcsr04sensor/1.2.0 already programmed library. 
+#use https://pypi.python.org/pypi/hcsr04sensor/1.2.0 already programmed library.
+from sklearn.ensemble import RandomForestRegresssor as rf
+import sqlite3
+import datetime
+
+def main():
+
+if __name__ == '__main__':
+    main()
+
+
+ def getPrediction():
+     model = getData()
+     rf.fit(model)
+     return rf.predict()
+
+ def getData():
+     conn = sqlite3.connect('placeholder.db')
+     cursor = conn.cursor
+
+     curTime = datetime.datetime.now().time()
+     c.execute('''SELECT * WHERE time<=? || time>?''', curTime, curTime - (0, 10, 0, 0, 0))
