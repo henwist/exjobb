6d478e0254aeb96c1a95b2b77893761c882a6f56
Rasmus Lundquist <Rasmus Lundquist>
2017-02-25 14:47:59 +0100

    Added more code to the visitor_counter to be used in the experiment

--- src_tugg/visitor_counter.py
+++ src_tugg/visitor_counter.py
@@ -2,17 +2,44 @@
 from sklearn.ensemble import RandomForestRegresssor as rf
 import sqlite3
 import datetime
-
+import src_busframework as bf
+import numpy
 
 class visitor_counter:
     conn
     cursor
+    visitor_count
 
     def __init__(db='placeholder.db'):
         self.conn = sqlite3.connect(db)
         self.cursor = self.conn.cursor
 
     def main():
+        ultrasoundsensor1 = new bf.i2c_bus(1)
+        ultrasoundsensor2 = new bf.i2c_bus(2)
+        tempsensor = new bf.i2c_bus(3)
+        pressensor = new bf.i2c_bus(4)
+
+        #Detta bör antagligen köras i en egen tråd om möjligt annars behöver vi nog hitta ett annat
+        #sätt att göra det på.
+        if (ultrasoundsensor1.activated):
+            values = ultrasoundsensor1.read()
+            values.append(',')
+            values.append(ultrasoundsensor2.read())
+            values.append(',')
+            values.append(tempsensor.read())
+            values.append(',')
+            values.append(pressensor.read())
+            values.append(',')
+            values.append(visitor_count++)
+            cursor.execute('''INSERT INTO visistor_data VALUES (?, ?, ?, ?, ?)''', values)
+
+        elif(ultrasoundsensor2.activated):
+            visitor_count--
+
+        # Sen behöver vi göra prediktioner regelbundet, kanske hårdkoda det till var tionde minut.
+        # Alternativt skulle vi kunna göra prediktioner efter var tionde besökare efter att tillräckligt
+        # många besökare anlänt för att vi ska kunna börja predicera.
 
     if __name__ == '__main__':
         main()
@@ -20,19 +47,17 @@ class visitor_counter:
 
      def getPrediction():
          model = getData()
-         rf.fit(model)
-         return rf.predict()
+         n_features = model.shape[0]
+         n_samples = model.shape[1]
+         esitmator = rf.RandomForestRegresssor(numpy.random.ranomstate(0), n_estimators = 1000)
+         estimator.fit(model)
+         return estimator.predict(n_features, n_samples)
 
      def getData():
          curTime = datetime.datetime.now().time()
          self.cursor.execute('''SELECT * WHERE time<=? || time>?''', curTime, curTime - (0, 10, 0, 0, 0))
 
 
-     def insertRow():
-         values = sensors.read()
-         self.cursor.execute('''INSERT INTO visitor_data VALUES (?,?,?)''', values)
-
-
      def initDb(name):
 
-         self.cursor.execute('''CREATE TABLE visitor_data(?, ?)''')
+         self.cursor.execute('''CREATE TABLE ?''', name)
