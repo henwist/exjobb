2dd4903949f5982f5a22d957b7f3fa2c9c326b3c
Rasmus Lundquist <Rasmus Lundquist>
2017-03-16 23:19:49 +0100

    Added functionality for fetching tables from the database via command line

--- src_busframework/cmd_tool.py
+++ src_busframework/cmd_tool.py
@@ -3,7 +3,7 @@ import argparse as ap
 
 
 parser = ap.ArgumentParser()
-parser.add_argument("-r", "--retrieve", help="Return all modules in the module database")
+parser.add_argument("-m", "--modules", help="Return all modules in a table which is specified as a paramater to this option, from the module database")
 parser.add_argument("-s","--send", help="Takes an sql statement and executes it on the database")
 parser.add_argument("db", help="The database on which operations will be executed")
 
@@ -13,4 +13,14 @@ if args.send:
     cur = con.cursor()
     cur.execute(args.send)
     con.commit()
+    con.close()
+
+
+if args.modules:
+    con = sq.connect(args.db)
+    cur = con.cursor()
+    query = 'SELECT * FROM' + ' ' + args.modules
+    cur.execute(query)
+    modules = cur.fetchall()
+    print modules
     con.close()
